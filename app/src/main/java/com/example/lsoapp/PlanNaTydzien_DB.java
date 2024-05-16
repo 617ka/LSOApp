092d8fc0f063fdbdab4729429081bf1f9ab82377
617ka <107540266+617ka@users.noreply.github.com>
2024-05-16 21:38:48 +0200

    Update PlanNaTydzien_DB.java

--- app/src/main/java/com/example/lsoapp/PlanNaTydzien_DB.java
+++ app/src/main/java/com/example/lsoapp/PlanNaTydzien_DB.java
@@ -6,6 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+//klasa abstrakcyjna zwracająca instancję bazy danych
 @Database(entities = {DodanieSluzby.class}, version = 1)
 public abstract class PlanNaTydzien_DB extends RoomDatabase {
 
